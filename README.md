551204b1c4890cd62fb65a391589e8b0b5fd1142
ckh2445 <79975403+ckh2445@users.noreply.github.com>
2022-03-13 20:58:06 +0900

    final

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 2020-03-13
 - 현재 컴퓨터의 HwProfileGuid 조회 추가
 - HwProfileGuid를 만들기 위해 https://www.uuidgenerator.net/version4 사이트를 크롤링 하여 Label에 표시 함수 추가 
+- 불러온 HwProfileGuid를 내 컴퓨터에 저장 시켜주는 함수 추가 
+- pyqt를 이용하여 gui 구성
--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import winreg
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5 import QtGui
-from PyQt5.QtCore import QObject, Qt, QThread, pyqtSignal
 import sys
 import requests
 from bs4 import BeautifulSoup
-
+import wmi   
 form_class = uic.loadUiType("my_design.ui")[0]
 
 class My_Window(QMainWindow, form_class): #design.Ui_mainWindow
     def __init__(self):
         super().__init__() 
         self.setupUi(self)
-        self.show_HWID()
         
+        self.c = wmi.WMI()
+        self.show_HWID()
+        self.m = self.c.Win32_PhysicalMedia()
+        
+        for item in self.m:
+            print(item)
         self.BT_Create.clicked.connect(self.Create_GUID_Clicked)
         self.BT_Change.clicked.connect(self.Change_GUID_Cliecked)
         #key = winreg.OpenKey( winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001',0, winreg.KEY_SET_VALUE)
 
-    def show_HWID(self):
+    def show_HWID(self): #label에 현재 HwProfileGuid display
         self.key = winreg.OpenKey( winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001',0, winreg.KEY_READ)
         (value,typevalue) = winreg.QueryValueEx(self.key,'HwProfileGuid')
         self.label_2.setText("HwProfileGuid")
-        self.label_4.setText(str(value))
+        self.label_4.setText( str(value) )
 
-    def Create_GUID_Clicked(self):
+    def Create_GUID_Clicked(self): #GUID 버튼 클릭 시 발생하는 이벤트
         self.url = "https://www.uuidgenerator.net/"
         self.response = requests.get(self.url)
         self.html = self.response.text
@@ -34,14 +37,19 @@ class My_Window(QMainWindow, form_class): #design.Ui_mainWindow
         
         self.label_6.setText(self.hw_list.get_text())
 
-    def Change_GUID_Cliecked(self):
+    def Change_GUID_Cliecked(self): #Change 버튼 클릭 시 발생하는 이벤트
         if self.label_6.text() == "":
-            QMessageBox.warning(self,"Warning","캐릭터를 찾을 수 없습니다.")
+            QMessageBox.warning(self,"Warning","Create HwProfileGuid")
         else:
-            pass
-        
-        
-
+            self.Create_GUID = self.label_6.text()
+            self.HwProfileGuid_key = winreg.OpenKey( winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001',0, winreg.KEY_SET_VALUE)
+            self.MachineGuid_key = winreg.OpenKey( winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Microsoft\\Cryptography',0, winreg.KEY_SET_VALUE)
+            winreg.SetValueEx(self.HwProfileGuid_key , 'HwProfileGuid', 0, winreg.REG_SZ, '{' + self.Create_GUID + '}')
+            winreg.SetValueEx(self.MachineGuid_key , 'MachineGuid', 0, winreg.REG_SZ, self.Create_GUID)
+            winreg.CloseKey(self.HwProfileGuid_key)
+            winreg.CloseKey(self.MachineGuid_key)
+            QMessageBox.about(self,"","Success")
+            
 class Warning(QMessageBox):
     def __init__(self):
         QMessageBox.__init__(self)
