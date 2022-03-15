68d828fe40b8ef2b9cd7826f4ed0aaf78680ab09
ckh2445 <79975403+ckh2445@users.noreply.github.com>
2022-03-15 20:43:52 +0900

    final
    final

--- main.py
+++ main.py
@@ -4,22 +4,38 @@ from PyQt5 import uic
 import sys
 import requests
 from bs4 import BeautifulSoup
-import wmi   
+#import wmi   
+import subprocess
+import regex as re
+import string
+import random
+
 form_class = uic.loadUiType("my_design.ui")[0]
 
 class My_Window(QMainWindow, form_class): #design.Ui_mainWindow
     def __init__(self):
         super().__init__() 
         self.setupUi(self)
+        self.network_interface_reg_path = r"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}"
+        self.transport_name_regex = re.compile("{.+}")
+        self.mac_address_regex = re.compile(r"([A-Z0-9]{2}[:-]){5}([A-Z0-9]{2})")
         
-        self.c = wmi.WMI()
+        # #self.c = wmi.WMI()
+        # #self.m = self.c.Win32_PhysicalMedia()
+        # for item in self.m:
+        #     print(item)
+        
+        #test
+        self.test = self.get_connected_adapters_mac_address()
+        print(self.test)
+        
+        #Hwid display
         self.show_HWID()
-        self.m = self.c.Win32_PhysicalMedia()
         
-        for item in self.m:
-            print(item)
+        #button 리스너
         self.BT_Create.clicked.connect(self.Create_GUID_Clicked)
         self.BT_Change.clicked.connect(self.Change_GUID_Cliecked)
+        self.BT_Create_Mac.clicked.connect(self.get_random_macaddress_Clicked)
         #key = winreg.OpenKey( winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\\CurrentControlSet\\Control\\IDConfigDB\\Hardware Profiles\\0001',0, winreg.KEY_SET_VALUE)
 
     def show_HWID(self): #label에 현재 HwProfileGuid display
@@ -50,13 +66,23 @@ class My_Window(QMainWindow, form_class): #design.Ui_mainWindow
             winreg.CloseKey(self.MachineGuid_key)
             QMessageBox.about(self,"","Success")
             
-class Warning(QMessageBox):
-    def __init__(self):
-        QMessageBox.__init__(self)
-        #self.setWindowFlags(Qt.Window | Qt.CustomizeWindowHint | Qt.WindowTitleHint | Qt.WindowCloseButtonHint | Qt.WindowStaysOnTopHint)
-        #self.setupUi(self)
-        #self.move(500,500)
+    def get_random_macaddress_Clicked(self):
+        print("test")
         
+    def get_connected_adapters_mac_address(self):
+        # make a list to collect connected adapter's MAC addresses along with the transport name
+        self.connected_adapters_mac = []
+        # use the getmac command to extract 
+        for self.potential_mac in subprocess.check_output("getmac").decode().splitlines():
+            # parse the MAC address from the line
+            mac_address = self.mac_address_regex.search(self.potential_mac)
+            # parse the transport name from the line
+            transport_name = self.transport_name_regex.search(self.potential_mac)
+            if mac_address and transport_name:
+                # if a MAC and transport name are found, add them to our list
+                self.connected_adapters_mac.append((mac_address.group(), transport_name.group()))
+        return self.connected_adapters_mac
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = My_Window()
